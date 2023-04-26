6521e5e6b8a62297ef7388de807d362d2172113e
Camilo Andrés Guevara Triana <c.guevarat@uniandes.edu.co>
2023-04-25 21:06:42 -0500

    Update main activity

--- app/src/main/java/com/example/mobilevynils/ui/MainActivity.kt
+++ app/src/main/java/com/example/mobilevynils/ui/MainActivity.kt
@@ -1,31 +1,51 @@
 package com.example.mobilevynils.ui
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+
 import com.example.mobilevynils.R
 import com.example.mobilevynils.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 
+import androidx.fragment.app.Fragment
+import com.example.mobilevynils.AlbumFragment
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    lateinit var bottomNav : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navView: BottomNavigationView = binding.navView
+        bottomNav = findViewById(R.id.nav_view) as BottomNavigationView
+        loadFragment(AlbumFragment())
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_albums -> {
+                    loadFragment(AlbumFragment())
+                    true
+                }
+                R.id.navigation_artists -> {
+                    loadFragment(ArtistaFragment())
+                    true
+                }
 
+                else -> {
+                    loadFragment(AlbumFragment())
+                    true
+                }
+            }
+        }
 
     }
 
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
 
 }
