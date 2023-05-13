0f08907fe0d53e2bc59fd92c9e5b37f15d5713a9
ELISA ANGULO <e.angulov@uniandes.edu.co>
2023-05-13 18:15:07 -0500

    Se sube la HU04 correspondiente al detalle de artistas, Verde

--- app/src/main/java/com/example/mobilevynils/models/Artista.kt
+++ app/src/main/java/com/example/mobilevynils/models/Artista.kt
@@ -1,19 +1,17 @@
 package com.example.mobilesvynilis.models
 
+import com.example.mobilevynils.models.Prize
 import java.util.Date
 
 
-data class Artista(
-    val id: Int,
-    val name: String,
-    val image: String,
-    val description: String,
-    val birthDate: String
-    /*
-    val albums: List<Album>,
-    val performerPrizes: List<PerformerPrize>,
-
-     */
+data class Artista (
+    val artistId: Int,
+    val name:String,
+    val image:String,
+    val description:String,
+    val birthDate:String,
+    val albums: List<Album>?,
+    val prizes: List<Prize>?
 )
 
 
